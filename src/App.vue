967c856abb2d9482338ca1b53996d6109600c6cd
Jonas Ullrich <jonas.ullrich@falcondev.de>
2022-01-15 16:42:32 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
+
+html,
+body {
+  height: 100%;
+}
+</style>

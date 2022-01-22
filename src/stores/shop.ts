58698af04e1f6d7100146acf49b067a99467b19d
Jonas Ullrich <jonas.ullrich@falcondev.de>
2022-01-22 11:26:53 +0100

    add Pinia Store

---
+++ src/stores/shop.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import { TProduct, TCartItem } from '@/types/ShopTypes'
+
+export const useScrollStateStore = defineStore('shop', {
+  state: () => ({
+    products: [] as TProduct[],
+    shoppingCart: [] as TCartItem[],
+  }),
+  getters: {
+    getProducts(): TProduct[] {
+      return this.products
+    },
+  },
+  actions: {
+    setProducts(items: TProduct[]) {
+      this.products = items
+    },
+  },
+})
---
+++ src/types/ShopTypes.ts
@@ -0,0 +1,10 @@
+export interface TProduct {
+  id: number
+  name: string
+  price: number
+}
+export interface TCartItem {
+  id: number
+  //   count: string
+  count: number
+}
